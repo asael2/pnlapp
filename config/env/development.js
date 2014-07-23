437002a0867c329636ab3d28a398117ecfd48800
ayf media <asael2@gmail.com>
2014-07-23 00:20:59 -0500

    Agregados API socials

--- config/env/development.js
+++ config/env/development.js
@@ -11,18 +11,18 @@ module.exports = {
         callbackURL: 'http://localhost:3000/#!/articles'
     },
     twitter: {
-        clientID: process.env.TWITTER_KEY || 'CONSUMER_KEY',
-        clientSecret: process.env.TWITTER_SECRET || 'CONSUMER_SECRET',
-        callbackURL: 'http://localhost:3000/auth/twitter/callback'
+        clientID: process.env.TWITTER_KEY || 'D6HXtaVQWpQqH4sgJMCY3QZyi',
+        clientSecret: process.env.TWITTER_SECRET || '0wdmvfMBDDBl7PjSNhPuTZ6W0r7nZ804eYxyEg4JPjuJ53nXKh',
+        callbackURL: 'http://localhost:3000/#!/articles'
     },
     google: {
-        clientID: process.env.GOOGLE_ID || 'APP_ID',
-        clientSecret: process.env.GOOGLE_SECRET || 'APP_SECRET',
+        clientID: process.env.GOOGLE_ID || '778869874780-h26u137oscm4qq46iacud32avd072rnn.apps.googleusercontent.com',
+        clientSecret: process.env.GOOGLE_SECRET || 'cz4VVe9Yi4RWLYu5_eugzQq_',
         callbackURL: 'http://localhost:3000/auth/google/callback'
     },
     linkedin: {
-        clientID: process.env.LINKEDIN_ID || 'APP_ID',
-        clientSecret: process.env.LINKEDIN_SECRET || 'APP_SECRET',
+        clientID: process.env.LINKEDIN_ID || '75iyy36awg1ms6',
+        clientSecret: process.env.LINKEDIN_SECRET || 'r5Nb9EPDPl0HPBSw',
         callbackURL: 'http://localhost:3000/auth/linkedin/callback'
     }
 };
