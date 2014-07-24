fbb0fa0e4e241bb39696e325dcad540679156409
ayf media <asael2@gmail.com>
2014-07-24 01:17:28 -0500

    Changes for ENVironments

--- public/dist/application.js
+++ public/dist/application.js
@@ -378,6 +378,7 @@ angular.module('users').controller('AuthenticationController', [
       });
     };
     $scope.signin = function () {
+      console.log($scope.credentials);
       $http.post('/auth/signin', $scope.credentials).success(function (response) {
         //If successful we assign the response to the global user model
         $scope.authentication.user = response;
