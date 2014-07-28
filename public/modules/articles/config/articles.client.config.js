6e25d51d532b6580ae59118b78bbab1de622ec0d
ayf media <asael2@gmail.com>
2014-07-28 14:20:15 -0500

    Menu with admin ok

--- public/modules/articles/config/articles.client.config.js
+++ public/modules/articles/config/articles.client.config.js
@@ -7,7 +7,10 @@ angular.module('articles').run(['Menus',
         Menus.addMenuItem('topbar', 'Articulos', 'articles', 'dropdown', '/articles(/create)?');
         Menus.addSubMenuItem('topbar', 'articles', 'Mis Articulos', 'learn');
         Menus.addSubMenuItem('topbar', 'articles', 'Todos los Articulos', 'articles');
-        // if (user.roles[1] === 'admin') Menus.addSubMenuItem('topbar', 'articles', 'Nuevo Articulo', 'articles/create');
+
+        Menus.addSubMenuItem('topbar', 'articles', ' - Admin', ' ', 'menuItemUIRoute', true, ['admin']);
+
         Menus.addSubMenuItem('topbar', 'articles', 'Agregar articulo', 'articles/create', 'menuItemUIRoute', true, ['admin']);
+        Menus.addSubMenuItem('topbar', 'articles', 'Agregar Modulo', 'emodule-create', 'menuItemUIRoute', true, ['admin']);
     }
 ]);
