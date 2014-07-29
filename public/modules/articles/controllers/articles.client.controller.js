371722a9f408829effc55c8ca86005cf399659bc
ayf media <asael2@gmail.com>
2014-07-28 23:33:21 -0500

    View for emodules implemented

--- public/modules/articles/controllers/articles.client.controller.js
+++ public/modules/articles/controllers/articles.client.controller.js
@@ -1,78 +1,98 @@
 'use strict';
 
-angular.module('articles').controller('ArticlesController', ['$scope', '$stateParams', '$location', 'Authentication', 'Articles', 'Users', '$http',
-    function($scope, $stateParams, $location, Authentication, Articles, Users, $http) {
+angular.module('articles').controller('ArticlesController', ['$scope', '$stateParams', '$location', 'Authentication', 'Articles', 'Users', '$http', 'Emodules',
+    function($scope, $stateParams, $location, Authentication, Articles, Users, $http, Emodules) {
 
         $scope.authentication = Authentication;
         $scope.user = Authentication.user;
+
         $scope.myUser = new Users($scope.user);
         $scope.myUser.userArticles = $scope.user.userArticles;
 
+        $scope.emodules = Emodules.query();
+        $scope.belongsToModule = [];
 
-    $scope.create = function() {
-        var article = new Articles({
-            title: this.title,
-            content: this.content,
-            arType: this.arType,
-            skills: this.skills,
-            resource: this.resource,
-            status: this.status,
-        });
-        article.$save(function(response) {
-            $location.path('articles/' + response._id);
-        }, function(errorResponse) {
-            $scope.error = errorResponse.data.message;
-        });
-
-        this.title = '';
-        this.content = '';
-        this.arType = '';
-        this.skills = '';
-        this.resource = '';
-        this.status = '';
-    };
-
-    $scope.remove = function(article) {
-        if (article) {
-            article.$remove();
-
-            for (var i in $scope.articles) {
-                if ($scope.articles[i] === article) {
-                    $scope.articles.splice(i, 1);
-                }
-            }
-        } else {
-            $scope.article.$remove(function() {
-                $location.path('articles');
+        $scope.create = function() {
+            var article = new Articles({
+                title: this.title,
+                content: this.content,
+                arType: this.arType,
+                skills: this.skills,
+                resource: this.resource,
+                status: this.status,
+                belongsToModule: this.belongsToModule
             });
+            article.$save(function(response) {
+                $location.path('articles/' + response._id);
+            }, function(errorResponse) {
+                $scope.error = errorResponse.data.message;
+            });
+
+            this.title = '';
+            this.content = '';
+            this.arType = '';
+            this.skills = '';
+            this.resource = '';
+            this.status = '';
+            this.belongsToModule = '';
+        };
+
+        $scope.remove = function(article) {
+            if (article) {
+                article.$remove();
+
+                for (var i in $scope.articles) {
+                    if ($scope.articles[i] === article) {
+                        $scope.articles.splice(i, 1);
+                    }
+                }
+            } else {
+                $scope.article.$remove(function() {
+                    $location.path('articles');
+                });
+            }
+        };
+
+        $scope.update = function() {
+            var article = $scope.article;
+
+            article.$update(function() {
+                $location.path('articles/' + article._id);
+            }, function(errorResponse) {
+                $scope.error = errorResponse.data.message;
+            });
+        };
+
+        $scope.find = function() {
+            $scope.articles = Articles.query();
+        };
+
+        $scope.findOne = function() {
+            $scope.article = Articles.get({
+                articleId: $stateParams.articleId
+            });
+        };
+
+        $scope.filterAlreadyAdded = function(item) {
+            // console.log("Filtrando " + item._id);
+            return ($scope.myUser.userArticles.indexOf(item._id) == -1);
+        };
+
+        $scope.selectionsChanged = function(elementId) {
+            if (inArray(elementId, $scope.belongsToModule)) {
+                console.log("ya existe en belongsToModule : " + $scope.belongsToModule)
+            } else {
+                $scope.belongsToModule.push(elementId);
+            }
+
+        };
+
+        function inArray(x, arr) {
+            for (var i = 0; i < arr.length; i++) {
+                if (x === arr[i]) return true;
+            }
+            return false;
         }
-    };
-
-    $scope.update = function() {
-        var article = $scope.article;
-
-        article.$update(function() {
-            $location.path('articles/' + article._id);
-        }, function(errorResponse) {
-            $scope.error = errorResponse.data.message;
-        });
-    };
-
-    $scope.find = function() {
-        $scope.articles = Articles.query();
-    };
-
-    $scope.findOne = function() {
-        $scope.article = Articles.get({
-            articleId: $stateParams.articleId
-        });
-    };
-
-    $scope.filterAlreadyAdded = function(item) {
-        // console.log("Filtrando " + item._id);
-        return ($scope.myUser.userArticles.indexOf(item._id) == -1);
-
-    };
 
     }
 ]);
--- public/modules/emodule/config/emodule.client.routes.js
+++ public/modules/emodule/config/emodule.client.routes.js
@@ -2,16 +2,20 @@
 
 //Setting up route
 angular.module('emodule').config(['$stateProvider',
-	function($stateProvider) {
-		// Emodule state routing
-		$stateProvider.
-		state('emodules', {
-			url: '/emodules',
-			templateUrl: 'modules/emodule/views/emodules.client.view.html'
-		}).
-		state('emodule-create', {
-			url: '/emodule-create',
-			templateUrl: 'modules/emodule/views/emodule-create.client.view.html'
-		});
-	}
+    function($stateProvider) {
+        // Emodule state routing
+        $stateProvider.
+        state('emodules-emodule', {
+            url: '/emodules/:emoduleId',
+            templateUrl: 'modules/emodule/views/emodules-emodule.client.view.html'
+        }).
+        state('emodules-list', {
+            url: '/emodules',
+            templateUrl: 'modules/emodule/views/emodules.client.view.html'
+        }).
+        state('emodule-create', {
+            url: '/emodule-create',
+            templateUrl: 'modules/emodule/views/emodule-create.client.view.html'
+        });
+    }
 ]);
