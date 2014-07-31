ed13d7a7a6981165f2b1b2b66f1f411be6fa5825
ayf media <asael2@gmail.com>
2014-07-31 18:07:53 -0500

    Odisea 2001

--- public/modules/emodule/controllers/emodules.client.controller.js
+++ public/modules/emodule/controllers/emodules.client.controller.js
@@ -6,77 +6,12 @@ angular.module('emodule').controller('EmodulesController', ['$scope', 'Emodules'
         $scope.findAll = function() {
             $scope.emodules = Emodules.query();
             $scope.articles = Articles.query();
-
-            $scope.getChildArticle = function(emodule, article) {
-
-                var emoduleId = emoduleId;
-                var article = article;
-
-                console.log("emodule: " + emodule._id + " :: " + article.belongsToModule.length);
-                for (var i = 0; article.belongsToModule.length; i++) {
-                    console.log(article[i])
-                }
-
-            }
-            // angular.forEach(article.belongsToModule, index, function() {
-            //     console.log(index);
-            // });
-            // var checkArt = function() {
-            //     Articles.articleByID(articleId);
-
-            // }
-            // console.log(checkArt.toJson());
-
-
-
-
-            // if (emoduleId == articleId) {
-            //     alert("hey")
-
-            // }
-
-            // var belongs2 = $scope.article._id;
-            // return articleId == belongs2
-
-            //  $scope.emodule = Emodules.get({
-            //     emoduleId: $stateParams.emoduleId
-            // });
-            // }
         };
-
-        $scope.findOne = function() {
-            $scope.emodule = Emodules.get({
-                emoduleId: $stateParams.emoduleId
-            });
-        };
-
-        // $scope.remove = function(emodule) {
-        //     alert("borrar : " + emodule)
-        //     // if (emodule) {
-        //     //     emodule.$remove();
-
-        //     //     for (var i in $scope.emodules) {
-        //     //         if ($scope.emodules[i] === emodule) {
-        //     //             $scope.emodules.splice(i, 1);
-        //     //         }
-        //     //     }
-        //     // } else {
-        //     //     $scope.emodule.$remove(function() {
-        //     //         $location.path('/emodules');
-        //     //     });
-        //     // }
-        // };
-
-        // $scope.update = function() {
-        //     var emodule = $scope.emodule;
-
-        //     emodule.$update(function() {
-        //         $location.path('emodule/' + emodule._id);
-        //     }, function(errorResponse) {
-        //         $scope.error = errorResponse.data.message;
+        // $scope.findOne = function() {
+        //     $scope.emodule = Emodules.get({
+        //         emoduleId: $stateParams.emoduleId
         //     });
         // };
-
     }
 ]).controller('EmoduleCreateController', ['$scope', '$stateParams', '$location', 'Authentication', 'Articles', 'Users', '$http', 'Emodules',
     function($scope, $stateParams, $location, Authentication, Articles, Users, $http, Emodules) {
@@ -84,16 +19,7 @@ angular.module('emodule').controller('EmodulesController', ['$scope', 'Emodules'
         $scope.authentication = Authentication;
         $scope.user = Authentication.user;
 
-
-        // $scope.articles = Articles.query();
-
-        // for (var article in $scope.articles(data)) {
-        //     console.log(data);
-
         $scope.create = function() {
-
-
-
             var emodule = new Emodules({
                 name: this.name,
                 parent: this.parent,
@@ -108,7 +34,6 @@ angular.module('emodule').controller('EmodulesController', ['$scope', 'Emodules'
             }, function(errorResponse) {
                 $scope.error = errorResponse.data.message;
             });
-
             this.name = '';
             this.parent = '';
             this.child = '';
