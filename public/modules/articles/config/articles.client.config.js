baf328a73b7476101dce9bb47e4191c5d12ad2c5
ayf media <asael2@gmail.com>
2014-08-06 03:54:19 -0500

    building menus

--- public/modules/articles/config/articles.client.config.js
+++ public/modules/articles/config/articles.client.config.js
@@ -7,15 +7,10 @@ angular.module('articles').run(['Menus',
         Menus.addMenuItem('topbar', 'Opciones', 'articles', 'dropdown', '/articles(/create)?');
         Menus.addSubMenuItem('topbar', 'articles', 'Mis Articulos', 'learn');
         Menus.addSubMenuItem('topbar', 'articles', 'Todos los Articulos', 'articles');
-
-        // Menus.addSubMenuItem('topbar', 'articles', ' :articulos:', ' ', 'menuItemUIRoute', true, ['admin']);
-
-        Menus.addSubMenuItem('topbar', 'articles', 'Agregar articulo', 'articles/create', 'menuItemUIRoute', true, ['admin']);
-
-        Menus.addSubMenuItem('topbar', 'articles', ' - modulos -', ' ', 'menuItemUIRoute', true, ['admin']);
-
         Menus.addSubMenuItem('topbar', 'articles', 'Todos los Modulos', 'emodules', 'menuItemUIRoute', true, ['user']);
-
+        // Menus.addSubMenuItem('topbar', 'articles', ' :articulos:', ' ', 'menuItemUIRoute', true, ['admin']);
+        Menus.addSubMenuItem('topbar', 'articles', ' - admin menu -', ' ', 'menuItemUIRoute', true, ['admin']);
+        Menus.addSubMenuItem('topbar', 'articles', 'Agregar articulo', 'articles/create', 'menuItemUIRoute', true, ['admin']);
         Menus.addSubMenuItem('topbar', 'articles', 'Agregar Modulo', 'emodule-create', 'menuItemUIRoute', true, ['admin']);
     }
 ]);
